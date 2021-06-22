53c497d18fc66f1eb7537a81cee8a46b4b1e68aa
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2021-06-22 04:29:01 -0300

    Aula 2 - React JS

--- ReactJS/letmeask/src/App.tsx
+++ ReactJS/letmeask/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 function App() {
-  return (
-    <h1>Hello Luara</h1>
-  );
+	return (
+		<BrowserRouter>
+			<AuthContextProvider>
+				<Route path="/" exact component={Home} />
+				<Route path="/rooms/new" component={NewRoom} />
+			</AuthContextProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
